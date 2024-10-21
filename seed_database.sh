af167e4bd08772a505a08d05add33de8247ed98a
Caleb Ford <caleb.ford.sto@gmail.com>
2024-10-21 11:33:29 -0500

    Login and Register functionality plus fixtures for seeding data

--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,7 @@ rm -rf ./volunteerapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations volunteerapi
 python3 manage.py migrate volunteerapi
-# python3 manage.py loaddata users
-# python3 manage.py loaddata tokens
-
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata organizations
+python3 manage.py loaddata volunteers
