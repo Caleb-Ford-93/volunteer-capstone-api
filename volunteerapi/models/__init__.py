ef88a4fe15376478ec17108053428ce8d6c310c9
Caleb Ford <caleb.ford.sto@gmail.com>
2024-10-21 15:29:34 -0500

    added models and fixtures to build and seed database

--- volunteerapi/models/__init__.py
+++ volunteerapi/models/__init__.py
@@ -1,2 +1,5 @@
+from .skill import Skill
 from .organization import Organization
 from .volunteer import Volunteer
+from .opportunity import Opportunity
+from .volunteer_opportunity import VolunteerOpportunity
